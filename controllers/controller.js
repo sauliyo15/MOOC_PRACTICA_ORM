c89147a031968eed6b41ed8f56828fbf23d1da37
sauliyo15 <gc.saul.7@gmail.com>
2023-10-24 12:00:58 +0200

    Leves correciones y excepciones - OK

--- controllers/controller.js
+++ controllers/controller.js
@@ -9,26 +9,38 @@ module.exports = function (models) {
 
     // Buscar todos los hospitales
     module.list_hospitals = async function() {
-        let hospitals = await Hospital.findAll();
-        return hospitals;
+        try {
+            return await Hospital.findAll();
+        } catch (error) {
+            return error;
+        }
     }
 
     // Filtra los hospitales por ciudad
     module.filterHospitalsByCity = async function (city) {
-        let hospitals = await Hospital.findAll({where: {city: city}});
-        return hospitals;
+        try {
+            return await Hospital.findAll({where: {city: city}});
+        } catch (error) {
+            return error;
+        }
     }
 
     // Buscar pacientes de un hospital ordenadors por el nombre (de la A a la Z)
     module.list_hospital_patients = async function(hospital_id) {
-        let patients = await Patient.findAll({where: {hospital_id: hospital_id}, order: [['name', 'ASC']]},);
-        return patients;
+        try {
+            return await Patient.findAll({where: {hospital_id: hospital_id}, order: [['name', 'ASC']]},);
+        } catch (error) {
+            return error;
+        }  
     }
 
     // Muestra la informacion de un paciente
     module.read = async function(patient_id) {
-        let patient = await Patient.findByPk(patient_id)
-        return patient;
+        try {
+            return await Patient.findByPk(patient_id);
+        } catch (error) {
+            return error;
+        }
     }
 
     // Crea un paciente en un hospital
@@ -38,38 +50,57 @@ module.exports = function (models) {
             surname: surname,
             dni: dni,
             hospital_id: hospital_id
+        };
+        try {
+            return await Patient.create(new_patient);
+        } catch (error) {
+            return error;
         }
-        return await Patient.create(new_patient);
     }
 
     // Actualiza un paciente
     module.update= async function(patient_id, name, surname, dni) {
-        let patient = await Patient.findByPk(patient_id);
-        patient.name = name;
-        patient.surname = surname;
-        patient.dni = dni;
-        return await patient.save();
+        try {
+            let patient = await Patient.findByPk(patient_id);
+            patient.name = name;
+            patient.surname = surname;
+            patient.dni = dni;
+            return await patient.save();
+        } catch (error) {
+            return error;
+        }
     }
 
     // Borra un paciente
     module.delete = async function(patient_id) {
-        let patient = await Patient.findByPk(patient_id);
-        return await patient.destroy();
+        try {
+            let patient = await Patient.findByPk(patient_id);
+            return await patient.destroy();
+        } catch (error) {
+            return error;
+        }
     }
 
     // Asigna un doctor y devuelve los datos del paciente
     module.assignDoctor = async function (patient_id, doctor_id) {
-        let patient = await Patient.findByPk(patient_id);
-        let doctor = await Doctor.findByPk(doctor_id);
-        await patient.addDoctor(doctor);
-        return patient;
+        try {
+            let patient = await Patient.findByPk(patient_id);
+            let doctor = await Doctor.findByPk(doctor_id);
+            await patient.addDoctor(doctor);
+            return patient;
+        } catch (error) {
+            return error;
+        }
     }
 
     // Muestras los medicos de un paciente
     module.showPatientDoctors = async function (patient_id) {
-        let patient = await Patient.findByPk(patient_id);
-        doctors = await patient.getDoctors();
-        return doctors;
+        try {
+            let patient = await Patient.findByPk(patient_id);
+            return await patient.getDoctors();
+        } catch (error) {
+            return error;
+        }
     }
 
 
